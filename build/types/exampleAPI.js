3a578c55c4cb712028a833f2ea07ecba6ea4d61f
AudinoPantoja <pantojabel@gmail.com>
2024-10-30 20:33:37 -0500

    Exercise_express

--- build/types/exampleAPI.js
+++ build/types/exampleAPI.js
@@ -1,9 +1,1 @@
-console.log('\n 5. Array libros con sus propiedades y uso filter para libros de un solo autor ');
-const books = [
-    { title: "Don Quijote de la Mancha", author: "Miguel de Cervantes Saavedra" },
-    { title: "Cien años de Soledad", author: "Gabriel Garcia Marquez" },
-    { title: "Crimen y castigo", author: "Fiódor Dostoyevski" }
-];
-const authorBooks = books.filter(book => book.author === "Fiódor Dostoyevski");
-console.log(authorBooks, 'es tu autor elegido');
 //# sourceMappingURL=exampleAPI.js.map
